ca28314fe64e3f8e3cb664ff508c3a83c5e54fe7
alabulei1 <vivian.xiage@gmail.com>
2023-09-01 00:16:12 -0700

    Create test.rs

---
+++ src/test.rs
@@ -0,0 +1,29 @@
+use github_flows::{
+    event_handler, get_octo, listen_to_event,
+    octocrab::models::{events::payload::EventPayload, reactions::ReactionContent},
+    GithubLogin,
+};
+
+#[no_mangle]
+#[tokio::main(flavor = "current_thread")]
+pub async fn on_deploy() {
+    // `some_login` must be authed in flows.network
+    listen_to_event(&GithubLogin::Default, "alabulei1", "a-test", vec!["issue_comment"]).await;
+    listen_to_event(&GithubLogin::Default, "flows-network", "docs", vec!["issue_comment"]).await;
+}
+
+#[event_handler]
+async fn handler(payload: EventPayload) {
+    if let EventPayload::IssueCommentEvent(e) = payload {
+        let comment_id = e.comment.id.0;
+
+        // installed app login
+        let octo = get_octo(&GithubLogin::Provided(String::from("some_login")));
+
+        let _reaction = octo
+            .issues("some_owner", "some_repo")
+            .create_comment_reaction(comment_id, ReactionContent::Rocket)
+            .await
+            .unwrap();
+    };
+}

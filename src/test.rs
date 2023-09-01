59cc82cb5c8d1390a4d70520e7ac96b9f3b15181
alabulei1 <vivian.xiage@gmail.com>
2023-09-01 00:40:42 -0700

    Update test.rs

--- src/test.rs
+++ src/test.rs
@@ -1,3 +1,5 @@
+use dotenv::dotenv;
+use flowsnet_platform_sdk::logger;
 use github_flows::{
     event_handler, get_octo, listen_to_event,
     octocrab::models::{events::payload::EventPayload, reactions::ReactionContent},
@@ -16,14 +18,19 @@ pub async fn on_deploy() {
 async fn handler(payload: EventPayload) {
     if let EventPayload::IssueCommentEvent(e) = payload {
         let comment_id = e.comment.id.0;
+        logger::init();
+    log::debug!("Running test");
 
         // installed app login
-        let octo = get_octo(&GithubLogin::Provided(String::from("some_login")));
+        let octo = get_octo(&GithubLogin::Default;
+
+            log::debug!("login");
 
         let _reaction = octo
             .issues("some_owner", "some_repo")
             .create_comment_reaction(comment_id, ReactionContent::Rocket)
             .await
             .unwrap();
+           log::debug!("success");
     };
 }
